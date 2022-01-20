4648b5374ab22a5e93c0a1d7e77d117d97e2c8f3
Yan Leandro <yanleandro658@gmail.com>
2022-01-20 09:12:40 +0100

    cambios

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+<br /><br /><br /><br /><br />
+<div class="footer"><strong>Yan Leandro</strong> | &copy; <?php echo date('Y'); ?>.</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Simple Project Management | </title>
+</head>
+<body>
+
+<style>
+.centered {
+    margin: 0;
+    position: absolute;
+    padding: 10%;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align:center;
+    color: white;
+    background-color: grey;
+    font-size: 20px;
+    border-top: 4px solid black;
+    border-bottom: 4px solid black;
+    
+}
+
+</style>
+
+<div class="centered">
+<h2>My Simple Project Management</h2>
+
+
+<a href="manager/clients.php" target="_new">Manager</a> | <a href="employee/tasks.php" target="_new"> Employee</a>
+</div>
+
+<div class="footer"><strong>Yan Leandro</strong> &copy; <?php echo date('Y'); ?>.</div>
+
+</body>
+</html>
